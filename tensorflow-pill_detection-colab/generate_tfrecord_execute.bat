7128b24db1a95a99b2d5dff3d60d21ce8abc8140
Zekeriyya Demirci <54662519+zekeriyya61@users.noreply.github.com>
2019-10-23 11:55:31 +0300

    Delete generate_tfrecord_execute.bat

--- tensorflow-pill_detection-colab/generate_tfrecord_execute.bat
+++
@@ -1,4 +0,0 @@
-
-python generate_tfrecord.py --csv_input=eraserImg/train_labels.csv --image_dir=eraserImg/train --output_path=eraserImg/train.record
-python generate_tfrecord.py --csv_input=eraserImg/test_labels.csv  --image_dir=eraserImg/test --output_path=eraserImg/test.record
-
